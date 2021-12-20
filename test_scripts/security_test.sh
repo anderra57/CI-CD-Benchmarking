cd07553246b4eb4b6ac0ef00176971c148f6cd4a
Kerman-Sanjuan <kermansanjuanmalax@gmail.com>
2021-12-20 17:19:11 +0100

    security_test.sh

--- test_scripts/security_test.sh
+++ test_scripts/security_test.sh
@@ -13,10 +13,14 @@ payloads=(";id" "|id" "a);id" "%0Acat%20/etc/passwd" "%0A/usr/bin/id" "%0Aid" "%
 # Iterate through the payloads
 for payload in "${payloads[@]}"
 do
+    echo $payload
     # Send the payload to the server
-    response=$(curl --silent -X GET $root_uri'something'$payload)
+    response=$(/usr/bin/curl -s -o /dev/null  http://localhost:3000/app/something;id)
+    echo $response
     if [[ $response == *"uid"* ]]; then
+        
         echo "RCE vulnerability exists"
         exit 1
     fi
+    echo "No he sido vulnerable"
 done
