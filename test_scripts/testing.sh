b0a08ac3cfa415bbae827b80ab1c7c628d799014
Adei Arias <adeiariasupv@gmail.com>
2021-12-20 16:35:31 +0100

    Tests finished, change PATH

--- test_scripts/testing.sh
+++ test_scripts/testing.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
 # Run both tests
-path=$(/usr/bin/pwd)
-/usr/bin/echo "Running API testing..."
-/usr/bin/bash $path/api_testing.sh "John Doe" "Hello World"
-exit_code=$(/usr/bin/echo $?)
+path=$(pwd)
+bash $path/api_testing.sh "John Doe" "Hello World"
+exit_code=$(echo $?)
 if [ $exit_code -eq 1 ]; then
-    /usr/bin/echo "API testing failed"
+    echo "API testing failed"
     exit 1
 fi
-/usr/bin/echo "Running security testing..."
-/usr/bin/bash $path/security_test.sh
-exit_code=$(/usr/bin/echo $?)
+bash $path/security_test.sh
+exit_code=$(echo $?)
 if [ $exit_code -eq 1 ]; then
-    /usr/bin/echo "Security testing failed"
+    echo "Security testing failed"
     exit 1
 fi
